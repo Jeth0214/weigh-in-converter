2335af39e2cb3ab57888aaca3d89ebe53d7ba92b
Jeth0214 <rolandjethrosuyom@yahoo.com>
2020-02-29 18:27:13 +0300

    fourth commit

--- weigh.js
+++ weigh.js
@@ -1,35 +1,60 @@
 let weighFromValue;
 let weighToValue;
-let test;
-let weighFrom = document.getElementById("weighFrom");
-let weighTo = document.getElementById("weighTo");
+//let weighFrom = document.getElementById("weighFrom");
+//let weighTo = document.getElementById("weighTo");
+let test = document.querySelectorAll(".test");
 let mass = document.getElementById("mass");
+let testOutput = [
+    {from: "kilograms", to: "ounce", per1 : 35.274},
+    {from: "kilograms", to: "pound", per1 : 2.20462},
+    {from: "pound", to: "kilogram", per1 : 0.453592},
+    {from: "pound", to: "ounce", per1 : 16},
+    {from: "ounce", to: "kilogram", per1 : 0.0283495},
+    {from: "ounce", to: "pound", per1 : 0.0625},
+]
 
-console.log(mass.value);
+function mode(){
+    for(let i = 0; i < test.length; i++){
+        test[i].addEventListener("change", function(){
+            weighFromValue = test[0].value;
+            weighToValue = test[1].value
+        });
+    };
+    console.log(weighFromValue);
+    console.log(weighToValue);
+}
+
+mode();
+/*
 //get the value of the mass unit to convert
-weighFrom.addEventListener("change", function (e) {
-    weighFromValue = weighFrom.value;
+ weighFrom.addEventListener("change", async function () {
+    weighFromValue = await weighFrom.value;
     console.log(weighFromValue);
 });
 
 
 //get the value of the  desired mass unit
-weighTo.addEventListener("change", function () {
-    weighToValue = weighTo.value;
+weighTo.addEventListener("change",async function () {
+    weighToValue = await weighTo.value;
     console.log(weighToValue);
 });
-
+*/
 
 //get the value of input
-mass.addEventListener("input", function () {
-    if (weighFromValue === "kilogram" && weighToValue == "ounces") {
-        test = mass.value;
-        let result = test * 2.20462;
-        document.getElementById("output").value = result;
-        console.log(test);
+mass.addEventListener("input", async function () {
+    await console.log(mass.value);
+    let massInput = await mass.value;
+    let result;
+    await testOutput.forEach( el => {
+        if(el.from === weighFromValue && el.to === weighToValue){
+            
+            result = massInput * el.per1;
+        }
+    })
+    document.getElementById("output").value = result;
+        console.log(massInput);
         console.log(result);
-    }
-})
+});
 
 
 
