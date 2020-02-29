e5bbffb1a9cdaa152d9dd1b1ce233f6e3aa178a5
Jeth0214 <rolandjethrosuyom@yahoo.com>
2020-02-29 15:36:59 +0300

    second commit

--- weigh.js
+++ weigh.js
@@ -1,11 +1,36 @@
 let weighFromValue;
 let weighToValue;
+let test;
 let weighFrom = document.getElementById("weighFrom");
 let weighTo = document.getElementById("weighTo");
+let mass = document.getElementById("mass");
 
-weighFrom.addEventListener("change", function(){
+console.log(mass.value);
+//get the value of the mass unit to convert
+weighFrom.addEventListener("change", function (e) {
     weighFromValue = weighFrom.value;
     console.log(weighFromValue);
-}, false);
+});
+
+
+//get the value of the  desired mass unit
+weighTo.addEventListener("change", function () {
+    weighToValue = weighTo.value;
+    console.log(weighToValue);
+});
+
+
+//get the value of input
+mass.addEventListener("input", function () {
+    if (weighFromValue === "kilogram" && weighToValue == "ounces") {
+        test = mass.value;
+        let result = test * 2.20462;
+        document.getElementById("output").value = result;
+        console.log(test);
+        console.log(result);
+    }
+})
+
+
 
 
