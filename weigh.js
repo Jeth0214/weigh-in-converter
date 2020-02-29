c6aeedffcf91a10f1c623de49747795d7e36dd15
Jeth0214 <rolandjethrosuyom@yahoo.com>
2020-02-29 21:06:13 +0300

    refactor

--- weigh.js
+++ weigh.js
@@ -2,45 +2,42 @@ let weighFromValue;
 let weighToValue;
 let test = document.querySelectorAll(".test");
 let mass = document.getElementById("mass");
+let output = document.getElementById("output");
+
 let testOutput = [
-    {from: "kilogram", to: "ounce", per1 : 35.274},
-    {from: "kilogram", to: "pound", per1 : 2.20462},
-    {from: "pound", to: "kilogram", per1 : 0.453592},
-    {from: "pound", to: "ounce", per1 : 16},
-    {from: "ounce", to: "kilogram", per1 : 0.0283495},
-    {from: "ounce", to: "pound", per1 : 0.0625},
+    { from: "kilogram", to: "ounce", per1: 35.274 },
+    { from: "kilogram", to: "pound", per1: 2.20462 },
+    { from: "pound", to: "kilogram", per1: 0.453592 },
+    { from: "pound", to: "ounce", per1: 16 },
+    { from: "ounce", to: "kilogram", per1: 0.0283495 },
+    { from: "ounce", to: "pound", per1: 0.0625 },
 ]
 
-//get the value of select button
-function mode(){
-    for(let i = 0; i < test.length; i++){
-        test[i].addEventListener("change", function(){
-            weighFromValue = test[0].value;
-            weighToValue = test[1].value
-        });
-    };
-    console.log(weighFromValue);
-    console.log(weighToValue);
-}
-
-mode();
+test.forEach(item => {
+    item.addEventListener("change", function () {
+        weighFromValue = test[0].value;
+        weighToValue = test[1].value
+        convertion();
+    })
+})
 
 
 //get the value of input and convert it
-mass.addEventListener("input", function () {
-     console.log(mass.value);
-    let massInput =  mass.value;
-    let result;
-     testOutput.forEach( el => {
-        if(el.from === weighFromValue && el.to === weighToValue){
-            
+mass.addEventListener("keyup", convertion);
+
+function convertion(){
+    let massInput = mass.value;
+    let result = 0;
+
+    testOutput.forEach(el => {
+        if (el.from === weighFromValue && el.to === weighToValue) {
             result = massInput * el.per1;
+        } else if (weighFromValue === weighToValue) {
+            result = massInput
         }
     })
-    document.getElementById("output").value = result;
-        console.log(massInput);
-        console.log(result);
-});
+    output.value = result;
+}
 
 
 
